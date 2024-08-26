e2040373b4b8082f8dad4211a817afd7887dff89
水色の記憶 <115365112+Mizuiro2@users.noreply.github.com>
2024-08-26 13:44:32 +0800

    Site updated: 2024-08-26 13:44:31

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1724650913475";
+const VERSION = "1724651065851";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
